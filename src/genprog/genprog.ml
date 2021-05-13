94f37484a9c55d95f76de22edb622f3058e4c244
chanhle <letonchanh@gmail.com>
2021-05-13 00:57:26 -0400

    reorganized

--- src/genprog/genprog.ml
+++ src/genprog/genprog.ml
@@ -11,25 +11,42 @@ let template = "src/genprog/template.c"
 let mba_src = "mba.c"
 let rand_fname = "rand"
 
-class annotate_mba_visitor fd vis mba_exp cnt = object(self)
+type v_args = {
+  v_mba_exp: Mba.exp;
+  v_iters: int;
+  v_base: int;
+}
+
+class annotate_mba_visitor fd (args: v_args) = object(self)
   inherit nopCilVisitor
 
+  val symtab = 
+    let vs = Mba.vars_of_exp args.v_mba_exp in
+    List.map (fun v -> (v, makeLocalVar fd v intType)) vs
+
+  method private mk_cil_mba_exp =
+    let get_vi v = List.assoc v symtab in
+    let cil_exp = Mba.cil_of_exp get_vi args.v_mba_exp in
+    ignore (E.log "MBA in Cil: %a\n" d_exp cil_exp);
+    cil_exp
+
   method private mk_rand_init_stmt vi =
+    (* mkStmtOneInstr (CM.mkCall ~av:(Some (var vi)) rand_fname []) *)
     let v_rand = makeTempVar fd intType in
     let rand_stmt = mkStmtOneInstr (CM.mkCall ~av:(Some (var v_rand)) rand_fname []) in
-    let assign_stmt = mkStmtOneInstr (Set (var vi, BinOp (Mod, CM.exp_of_vi v_rand, integer 10, intType), !currentLoc)) in
+    let assign_stmt = mkStmtOneInstr (Set (var vi, BinOp (Mod, CM.exp_of_vi v_rand, integer args.v_base, intType), !currentLoc)) in
     CM.mk_block_stmt [rand_stmt; assign_stmt]
 
   method private mk_mba_stmt =
     let v_mba = makeTempVar fd intType in
-    v_mba, mkStmtOneInstr (Set (var v_mba, mba_exp, !currentLoc))
+    v_mba, mkStmtOneInstr (Set (var v_mba, self#mk_cil_mba_exp, !currentLoc))
 
   method vstmt (s: stmt) =
     let action s =
       match s.skind with
       | Return _ ->
         let v_cnt = makeTempVar fd intType in
-        let v_cnt_init_stmt = mkStmtOneInstr (Set (var v_cnt, integer cnt, !currentLoc)) in
+        let v_cnt_init_stmt = mkStmtOneInstr (Set (var v_cnt, integer args.v_iters, !currentLoc)) in
         (* Loop condition *)
         let bool_typ = TInt (IBool, []) in
         (* let loop_cond = UnOp (LNot, BinOp (Gt, CM.exp_of_vi v_cnt, zero, bool_typ), bool_typ) in *)
@@ -38,17 +55,12 @@ class annotate_mba_visitor fd vis mba_exp cnt = object(self)
         let loop_cond_stmt = mkStmt (If (loop_cond, break_blk, mkBlock [], !currentLoc)) in
         let decr_stmt = mkStmtOneInstr (Set (var v_cnt, BinOp (MinusA, CM.exp_of_vi v_cnt, one, intType), !currentLoc)) in
         (* Loop body *)
+        let vis = List.map (fun (_, vi) -> vi) symtab in
         let rand_init_stmts = List.map self#mk_rand_init_stmt vis in
         let v_mba, mba_stmt = self#mk_mba_stmt in
         let mba_loop_body = mkBlock ([loop_cond_stmt; decr_stmt] @ rand_init_stmts @ [mba_stmt]) in
         let mba_loop = mkStmt (Loop (mba_loop_body, !currentLoc, None, None)) in
         CM.mk_block_stmt [v_cnt_init_stmt; mba_loop; s]
-      (* | Loop (blk, _, s1, s2) ->
-        let d_stmt_opt = fun _ s -> match s with | None ->  nil | Some s -> d_stmt () s in
-        ignore (E.log "blk: %a" d_block blk);
-        ignore (E.log "s1: %a" d_stmt_opt s1);
-        ignore (E.log "s2: %a" d_stmt_opt s2);
-        s *)
       | _ -> s
     in
     ChangeDoChildrenPost (s, action)
@@ -91,13 +103,17 @@ let () =
 
     let mba_str = Sys.argv.(1) in
     let cnt = int_of_string Sys.argv.(2) in
+    let base = int_of_string Sys.argv.(3) in
 
     try
       let lexbuf = Lexing.from_string mba_str in
       let mba_exp = Mbaparser.mba_exp Mbalexer.tokenizer lexbuf in
-      let vs = Mba.vars_of_exp mba_exp in
       ignore (E.log "MBA: %s\n" (Mba.string_of_exp mba_exp));
-      ignore (E.log "MBA vars: %s\n" (CM.string_of_list vs));
+
+      let vargs = { 
+        v_mba_exp = mba_exp; 
+        v_iters = cnt; 
+        v_base = base } in
       
       (* Gen MBA Statement *)
       let ast = Frontc.parse template () in
@@ -108,13 +124,8 @@ let () =
       ast.globals <- remove_missing_proto_externs_and_builtin_functions ast.globals;
 
       let main_fd = CM.find_fun ast "main" in
-      let vis = List.map (fun v -> makeLocalVar main_fd v intType) vs in
-      let symtab = Hashtbl.create (List.length vs) in
-      let () = List.iter2 (fun v vi -> Hashtbl.add symtab v vi) vs vis in
-      let get_vi = Hashtbl.find symtab in
-      let cil_exp = Mba.cil_of_exp get_vi mba_exp in
-      ignore (E.log "MBA in Cil: %a\n" d_exp cil_exp);
-      ignore (visitCilFunction (new annotate_mba_visitor main_fd vis cil_exp cnt) main_fd);
+      
+      ignore (visitCilFunction (new annotate_mba_visitor main_fd vargs) main_fd);
       CM.writeSrc mba_src ast
     with e ->
       let msg = Printexc.to_string e
