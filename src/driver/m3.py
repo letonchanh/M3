21b83da93b9ea625ea489063f1561c71acdb2920
chanhle <letonchanh@gmail.com>
2021-05-14 21:17:10 -0400

    wrap Dig's methods

--- src/driver/m3.py
+++ src/driver/m3.py
@@ -10,6 +10,29 @@ sys.path.append(dig_path)
 import dig
 import alg as dig_alg
 import settings as dig_settings
+import helpers.miscs as dig_miscs
+import helpers.vcommon as dig_vcommon
+import sage.all
+
+mlog = dig_vcommon.getLogger(__name__, dig_settings.logger_level)
+
+class Miscs(dig_miscs.Miscs):
+    @classmethod
+    def init_terms(cls, vs, deg, rate):
+        assert vs, vs
+        assert deg >= 1, deg
+        assert rate >= 0.1, rate
+
+        parent = dig_miscs.Miscs
+
+        terms = parent.get_terms([sage.all.var(v) for v in vs if v != "mba"], deg) # + [sage.all.var("mba")]
+
+        template, uks = parent.mk_template(terms, sage.all.var("mba"), retCoefVars=True)
+        
+        mlog.debug(f"template: {template}")
+
+        n_eqts_needed = int(rate * len(uks))
+        return terms, template, uks, n_eqts_needed
 
 if __name__ == "__main__":
     dig_settings.DO_EQTS = True
@@ -19,6 +42,8 @@ if __name__ == "__main__":
 
     inp = Path("../../mba.tcs")
     seed = round(time.time(), 2)
+
+    dig_miscs.Miscs.init_terms = Miscs.init_terms
     solver = dig_alg.DigTraces(inp, None)
     solver.start(seed=seed, maxdeg=2)        
     dig.killchildren(os.getpid())
